3908f5a6fbf3ad5cb1c50850578ff31ad3970864
Angela Jiang <to.ajiang@gmail.com>
2019-05-13 18:58:13 -0600

    Change scripts structure

--- scripts/fast_gradual/run_imagenet.sh
+++ scripts/fast_gradual/run_imagenet.sh
@@ -1,10 +1,11 @@
 expname=$1
 SAMPLING_MIN=$2
-BATCH_SIZE=$3
-PROB_STRATEGY=$4
-LOSS=$5
-MAX_HISTORY_LEN=$6
+NET="resnet"
+BATCH_SIZE=128
+PROB_STRATEGY="relative-cubed"
+LOSS="cross"
 
+START_EPOCH=1
 NUM_TRIALS=1
 
 set -x
@@ -14,16 +15,16 @@ ulimit -a
 
 EXP_PREFIX=$expname
 SAMPLING_STRATEGY="sampling"
-LR=0.001
-DECAY=0.0005
-MAX_NUM_BACKPROPS=1000000
+LR="data/config/lr_sched_imagenet_gradual"
+DECAY=0.0001
+MAX_NUM_BACKPROPS=25802100
 SEED=1337
-NET=LeNet-5
+DATA_DIR="/proj/BigLearning/ahjiang/datasets/imagenet-data"
 
 EXP_NAME=$EXP_PREFIX"_"$PROB_STRATEGY"_"$LOSS
 
-mkdir "/proj/BigLearning/ahjiang/output/mnist/"
-OUTPUT_DIR="/proj/BigLearning/ahjiang/output/mnist/"$EXP_NAME
+mkdir "/proj/BigLearning/ahjiang/output/imagenet/"
+OUTPUT_DIR="/proj/BigLearning/ahjiang/output/imagenet/"$EXP_NAME
 PICKLE_DIR=$OUTPUT_DIR/pickles
 mkdir $OUTPUT_DIR
 mkdir $PICKLE_DIR
@@ -33,18 +34,19 @@ git rev-parse HEAD &> $OUTPUT_DIR/sha
 for i in `seq 1 $NUM_TRIALS`
 do
 
-  OUTPUT_FILE="sampling_mnist_"$NET"_"$SAMPLING_MIN"_"$BATCH_SIZE"_"$MAX_HISTORY_LEN"_"$DECAY"_trial"$i"_seed"$SEED"_v2"
-  PICKLE_PREFIX="sampling_mnist_"$NET"_"$SAMPLING_MIN"_"$BATCH_SIZE"_"$MAX_HISTORY_LEN"_"$DECAY"_trial"$i"_seed"$SEED
+  OUTPUT_FILE="sampling_imagenet_"$NET"_"$SAMPLING_MIN"_"$BATCH_SIZE"_0.0_"$DECAY"_trial"$i"_seed"$SEED"_v2"
+  PICKLE_PREFIX="sampling_imagenet_"$NET"_"$SAMPLING_MIN"_"$BATCH_SIZE"_0.0_"$DECAY"_trial"$i"_seed"$SEED
 
   echo $OUTPUT_DIR/$OUTPUT_FILE
 
   time python main.py \
     --prob-strategy=$PROB_STRATEGY \
-    --max-history-len=$MAX_HISTORY_LEN \
-    --dataset=mnist \
     --prob-loss-fn=$LOSS \
-    --sb-start-epoch=1 \
+    --dataset=imagenet \
+    --datadir=$DATA_DIR \
     --sb-strategy=$SAMPLING_STRATEGY \
+    --sb-start-epoch=$START_EPOCH \
+    --net=$NET \
     --batch-size=$BATCH_SIZE \
     --decay=$DECAY \
     --max-num-backprops=$MAX_NUM_BACKPROPS \
@@ -53,7 +55,7 @@ do
     --sampling-min=$SAMPLING_MIN \
     --augment \
     --seed=$SEED \
-    --lr $LR &> $OUTPUT_DIR/$OUTPUT_FILE
+    --lr-sched $LR &> $OUTPUT_DIR/$OUTPUT_FILE
 
   let "SEED=SEED+1"
 done
