d9c2c017bd8eaaa2e97d5699c289b7dd04c48293
Angela Jiang <to.ajiang@gmail.com>
2019-05-16 15:41:22 -0600

    Revert "Add backward/forward timing info"
    This reverts commit 3346f983c0c9696770874fd01d16d9651fc310a7.

--- lib/backproppers.py
+++ lib/backproppers.py
@@ -1,5 +1,4 @@
 import torch
-from timeit import default_timer as timer
 import torch.nn as nn
 
 class PrimedBackpropper(object):
@@ -64,10 +63,7 @@ class BaselineBackpropper(object):
 
         # Run forward pass
         # Necessary if the network has been updated between last forward pass
-        start = timer()
         outputs = self.net(data) 
-        end = timer()
-        print("forwards {}".format(end - start))
         losses = self.loss_fn(reduce=False)(outputs, targets)
 
         # Add for logging selected loss
@@ -79,14 +75,8 @@ class BaselineBackpropper(object):
 
         # Run backwards pass
         self.optimizer.zero_grad()
-        start = timer()
         loss.backward()
-        end = timer()
-        print("backwards {}".format(end - start))
-        start = timer()
         self.optimizer.step()
-        end = timer()
-        print("update {}".format(end - start))
 
         print("total_norm {}".format(self.total_norm))
 
